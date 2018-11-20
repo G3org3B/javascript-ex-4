47f43b0b2f20c20ecbf478cb35333576c7e0a384
G3org3B <vbt.george@gmail.com>
2018-11-20 10:19:17 +0100

    main complete

--- mainpalindrom.js
+++ mainpalindrom.js
@@ -3,3 +3,25 @@
 // Creare Una funziona per capire se la parola è palindroma
 
 var word = prompt("Inserisci una parola");
+console.log(word);
+
+var parolaReverse = "";
+console.log(parolaReverse);
+
+// Creare il ciclo per contare al contario
+
+for (var i = word.length - 1; i >= 0; i--) {
+
+  parolaReverse += word[i];
+
+  console.log(parolaReverse);
+}
+
+if (word == parolaReverse) {
+
+  document.writeln('PAROLA PALINDROMA')
+
+} else {
+
+  document.writeln('PAROLA NON PALINDROMA')
+}
